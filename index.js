c761ff1ec83d31acb9176c6b1a1463dc6dfcba51
symonogwe <symonogwe@gmail.com>
2023-05-29 06:27:57 +0300

    Create operate function and operator variables

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ function addition(...input) {
   return sum;
 }
 
-console.log(addition(1, 2, 40, 63));
-
 function subtraction(...input) {
     const subtract = input.reduce((accumulator, currentValue) => {
         return accumulator - currentValue;
     });
     return subtract;
 }
-
-console.log(subtraction(190, 34, 2, 16));
 
 function multiplication(...input) {
     const multiply = input.reduce((accumulator, currentValue) => {
@@ -25,8 +21,6 @@ function multiplication(...input) {
     return multiply;
 }
 
-console.log(multiplication(23, 10, 5));
-
 function division(...input) {
     const divide = input.reduce((accumulator, currentValue) => {
         return accumulator / currentValue;
@@ -34,4 +28,25 @@ function division(...input) {
     return divide;
 }
 
-console.log(division(300, 2, 10));
+// 3 variables for numbers and operator
+let number1 = null;
+let number2 = null;
+let operator = null;
+
+// operate function
+function operate(number1, number2, operator) {
+    if (operator === "+") {
+        return addition(number1, number2);
+    }
+    if (operator === "-") {
+        return subtraction(number1, number2);
+    }
+    if (operator === "*") {
+        return multiplication(number1, number2);
+    }
+    if (operator === "/") {
+        return division(number1, number2);
+    }
+}
+
+console.log(operate(900, 10, "-"));
